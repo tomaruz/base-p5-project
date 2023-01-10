dc17fe51223886321864d0fe06f9ca677f75287b
tomaruz <tomaruz2002@gmail.com>
2023-01-10 16:25:34 +0000

    experimentaion 2

--- js/sketch.js
+++ js/sketch.js
@@ -1,41 +1,66 @@
 let x = 0;
 let y = 0;
 let theta = 0;
-let inc = 0.05;
+let inc = 0.1;
 let offset = 0; // the amount offset from beginning of wave
 let amplitude;
 let frequency;
 let maxAngle;
+let num = 60;
+let mx = [];
+let my = [];
 function setup() {
     // when using while, must give an end or it will go on forever
-    amplitude = windowHeight / 4;
-    frequency = windowWidth / 4;
+    amplitude = windowHeight;
+    frequency = windowWidth;
     maxAngle = (windowWidth / frequency) * TWO_PI;
+    createCanvas(720, 400);
+    noStroke();
+    fill(255, 153);
+    for (let i = 0; i < num; i++) {
+      mx.push(i);
+      my.push(i);
+    }
 }
 
 // draw function executes every frame
 function draw() {
+    stroke(1);
+    noFill();
+
     createCanvas(windowWidth, windowHeight);
-    noStroke();
-    let length = 20;
+
+    let which = frameCount % num;
+    mx[which] = mouseX;
+    my[which] = mouseY;
+  
+    for (let i = 0; i < num; i++) {
+      // which+1 is the smallest (the oldest in the array)
+      let index = (which + 1 + i) % num;
+      ellipse(mx[index], my[index], i, i);
+    }
+    let length = 10;
     while (theta < maxAngle + offset) {
-        // amplitude = sin(theta - offset) * (windowHeight/2);
-        amplitude = ((theta-offset)/maxAngle) * (windowHeight/2);
-        for (i = length; i > 0; i--) {
-            y = sin(theta - (i * 0.05)) * amplitude;
-            fill(230, 230 - (230/length * i), 250);
-            square(x, y + height * 0.5, 16);
-            // ellipse(x, y + height * 0.5, 20);
+        // amplitude = sin(theta - offset) * (windowHeight*0.5);
+        amplitude = ((theta - offset) / maxAngle) * (windowHeight / 2);
+        for (let i = length; i > 0; i--) {
+            y = sin(theta - (i * 0.08)) * amplitude;
+            // fill(230, 230 - (230 / length * i), 100);
+            square(x, y + height * 0.5, 20);
+            // ellipse(x, y + height / 2, 10);
         }
-        for (i = length; i > 0; i--) {
-            y = cos(theta - (i * 0.08)) * amplitude;
-            fill(255, 255 - (255/length * i), 0)
-            ellipse(x, y + height * 0.5, 10);
+        for (let i = length; i > 0; i--) {
+            y = cos(theta - (i * 50)) * amplitude;
+            // fill(0, 0, 255 - (255 / length * i))
+            ellipse(x, y + height / 2, 10);
             // square(x, y + height * 0.5, 16);
         }
-        theta += 0.2;
-        x = ((theta - offset)/maxAngle) * windowWidth;
+        theta += 0.08;
+        x = ((theta - offset) / maxAngle) * windowWidth;
+        
     }
+    
+
     offset += inc;
     theta = offset;
 }
