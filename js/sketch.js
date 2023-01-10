b4e50a8a67c6d90874e58d1ea2c68d15e15e4477
tomaruz <tomaruz2002@gmail.com>
2023-01-10 11:46:22 +0000

    basic animated sine wave

--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,31 @@
+let x = 0;
+let y = 0;
+let theta = 0;
+let inc = 0.05;
+let offset = 0; // the amount offset from beginning of wave
+let amplitude;
+let frequency;
+let maxAngle;
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    let x = 0;
-    let y = 0;
-    let theta = 0;
-    let amplitude = windowHeight/2;
-    let frequency = windowWidth/4;
-    let maxAngle = (windowWidth/frequency) * TWO_PI;
     // when using while, must give an end or it will go on forever
-    while(theta < maxAngle){
-        y = sin(theta) * 200;
-    // 16 is the diameter of the circle
-        ellipse(x, y + height*0.5, 16);
-        theta += 0.2;
-        x = (theta/maxAngle) * windowWidth;
-    }
+    amplitude = windowHeight / 2;
+    frequency = windowWidth / 4;
+    maxAngle = (windowWidth / frequency) * TWO_PI;
 }
+
+// draw function executes every frame
 function draw() {
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+    // fill(255, 0 , 0)
+    while (theta < maxAngle + offset) {
+        y = sin(theta) * 100;
+        // 16 is the diameter of the circle
+        ellipse(x, y + height * 0.5, 10);
+        theta += 0.2;
+        x = ((theta - offset) / maxAngle) * windowWidth;
+    }
+    offset += inc;
+    theta = offset;
+
 }
