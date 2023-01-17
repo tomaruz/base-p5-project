32035c96cbad35f86a6f01bfc065d861367e2814
tomaruz <tomaruz2002@gmail.com>
2023-01-17 10:45:42 +0000

    piet mondrian

--- js/grid1.js
+++ js/grid1.js
@@ -3,26 +3,33 @@ function setup() {
     const CELL_SIZE = 50;
     createCanvas(800, 800);
     background(0)
-    noStroke();
     // let x and let y need to be outside the loop or they will get reset to 0 every time
+    // VARIABLES
     let x = 0;
     let y = 0;
-    let xVariation = 5;
-    let yVariation = 5;
-    let widthVariation = 5;
-    let heightVariation = 20;
+    let xVariation = 0;
+    let yVariation = 0;
+    let widthVariation = 0;
+    let heightVariation = 0;
+    let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
+    let currentWidth = random(sizes);
+    let currentHeight = random(sizes);
     // creating a color array
-    let colours = [color('#251f3a'), color('#deb210'),color('#dbd9d7'), color('#ce3219'), color('#324494')];
-    while(y < height) {
-        while(x < width){
+    let colours = [color('#251f3a'), color('#deb210'), color('#dbd9d7'), color('#ce3219'), color('#324494')];
+    strokeWeight(5);
+    while (y < height) {
+        while (x < width) {
             // pass the array though random
             fill(random(colours));
-            rect(x+random(-xVariation, xVariation),y + random(-yVariation, yVariation), CELL_SIZE + random(-heightVariation, heightVariation));
-            x+= CELL_SIZE;
+            // rect(x + random(-xVariation, xVariation), y + random(-yVariation, yVariation), CELL_SIZE + random(-heightVariation, heightVariation));
+            rect(x,y, currentWidth, currentHeight);
+            x += currentWidth;
+            currentWidth = random(sizes);
         }
-        y += CELL_SIZE;
-        x=0;
-}
+        y += currentHeight;
+        currentHeight = random(sizes);
+        x = 0;
+    }
 }
 function draw() {
 
