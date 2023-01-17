1c754469f211cbeaa2a34d4dfaa9b826bd140e87
tomaruz <tomaruz2002@gmail.com>
2023-01-17 14:36:17 +0000

    updated

--- js/grid2.js
+++ js/grid2.js
@@ -1,5 +1,7 @@
 // an empty array
 let textures = [];
+
+
 function preload() {
     // the number of textures I have
     let texturesNum = 4;
@@ -10,19 +12,31 @@ function preload() {
     }
 }
 function setup() {
+    let colours = [color('#251f3a'), color('#deb210'), color('#dbd9d7'), color('#ce3219'), color('#324494')];
     createCanvas(800, 800);
-    const CELL_SIZE = 80;
+    const CELL_SIZE = 20;
     let x = 0;
     let y = 0;
+    let xVariation = 20;
+    let yVariation = 20;
+    let widthVariation = 0;
+    let heightVariation = 0;
+    let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 4];
+    let currentWidth = random(sizes);
+    let currentHeight = random(sizes);
     noStroke();
     while (y < height) {
         // fill is for shapes, so we use tint
         while (x < width) {
-            tint(random(255))
+            tint(random(colours));
             let t = random(textures);
-            image(t, x, y, CELL_SIZE)
+            image(t, x + random(-xVariation, xVariation), y + random(-yVariation, yVariation), CELL_SIZE + random(-widthVariation, widthVariation), CELL_SIZE + random(-heightVariation, heightVariation));
+            x += currentWidth;
+            let availableWidth = width - x;
+            currentWidth = random(sizes);
             x += CELL_SIZE;
         }
+        
         y += CELL_SIZE;
         x = 0;
     }
