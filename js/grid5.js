665979d8133ea31a779916b568ac299c1bfe86d6
tomaruz <tomaruz2002@gmail.com>
2023-03-13 21:03:32 +0000

    updates

--- js/grid5.js
+++ js/grid5.js
@@ -1,75 +1,51 @@
 const CELL_SIZE = 100;
 let x = 0;
 let y = 0;
+let xVariation = 0;
+let yVariation = 0;
+let widthVariation = 0;
+let heightVariation = 0;
+let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 4];
+let currentWidth = random(sizes);
+let currentHeight = random(sizes);
 
-
+noStroke();
 
 function setup() {
-
-    let colours = [color('#11304b'), color('#e1dbb4'), color('#ed9033'), color('#f9c554')];
+    let colours = [color('#251f3a'), color('#deb210'), color('#dbd9d7'), color('#ce3219'), color('#324494')];
     createCanvas(800, 800);
 
     while (y < height) {
-
+        // fill is for shapes, so we use tint
         while (x < width) {
-
-            fill(color('#cb252b'))
-            noStroke();
+            fill(random(colours));
+            stroke(255)
+            strokeWeight(5)
             rect(x, y, 100, 100);
 
-            fill(random(colours));
             noStroke();
+            fill(random(colours));
+            // ellipse(x, y, 70)
 
-
-
-            if (random(1) > 0.5) {
-
-                fill(random(colours));
-                noStroke();
-                arc(x + 100, y, 200, 200, radians(90), radians(180));
+            if(random(1) > 0.5) //0.5 is the probability 
+            {
+                arc(x + 50, y + 50, 80, 80, 0, PI);
+            }
+            else{
+                triangle(x + 20, y + 75, x + 50, y + 25, x + 80, y + 75);
             }
 
-            else if (random(1) > 0.5) {
-
-                fill(random(colours));
-                noStroke();
-                arc(x, y, 200, 200, radians(0), radians(90));
-
-            }
-
-            else if (random(1) > 0.25) {
-
-                fill(random(colours));
-                noStroke();
-                arc(x, y + 100, 200, 200, radians(-90), radians(-360));
-
-            }
-
-            else {
-
-                arc(x + 100, y + 100, 200, 200, radians(180), radians(-90));
-
-            }
-
-
-            // if (random(1) > 0.25) {
+            // arc(x + 50, y + 50, 80, 80, 0, PI);
             // fill(random(colours));
-            // noStroke();    
-            // arc(x+100, y+100, 200, 200, radians(180), radians(-90));
-            // }else{
-            // fill(random(colours));
-            // noStroke();    
-            // arc(x, y+100, 200, 200, radians(-90), radians(-360));
-            // }
 
-
+            // triangle(x + 20, y + 75, x + 50, y + 25, x + 80, y + 75);
             x += CELL_SIZE;
         }
 
         y += CELL_SIZE;
         x = 0;
     }
-
+    
 }
 
 function draw() {
