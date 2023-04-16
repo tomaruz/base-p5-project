7ce1b546b8456998e56c869c84c5053f2f030dce
tomaruz <tomaruz2002@gmail.com>
2023-04-16 14:55:17 +0100

    noupdates

--- js/grid3.js
+++ js/grid3.js
@@ -1,56 +1,55 @@
 function setup() {
-    //the width and the hight of indivual cell
+    // The width and height of individual cell
     const CELL_SIZE = 100;
     createCanvas(800, 800);
-    background(0)
+    background(0);
     // let x and let y need to be outside the loop or they will get reset to 0 every time
     // VARIABLES
     let x = 0;
     let y = 0;
-    let xVariation = 0;
-    let yVariation = 0;
-    let widthVariation = 0;
-    let heightVariation = 0;
-    let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
+
+    let sizes = [CELL_SIZE, CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3, CELL_SIZE * 4];
+    let currsize = random(sizes);
+    let rectH = currsize;
+    let currsize1 = random(sizes);
     let currentWidth = random(sizes);
     let currentHeight = random(sizes);
-    noStroke();
     // creating a color array
     let colours = [color('#fbeee6'), color('#ffe5d8'), color('#ffcad4'), color('#f3abb6'), color('#9f8189')];
-    strokeWeight(10);
+
     while (y < height) {
         while (x < width) {
-          
-            // pass the array though random
-            fill(random(colours));
-            stroke(2)
-            // rect(x + random(-xVariation, xVariation), y + random(-yVariation, yVariation), CELL_SIZE + random(-heightVariation, heightVariation));
-            rect(x,y, currentWidth, currentHeight);
 
+            // pass the array through random
             fill(random(colours));
-            // rect(x + random(-xVariation, xVariation), y + random(-yVariation, yVariation), CELL_SIZE + random(-heightVariation, heightVariation));
-            rect(x+(currentWidth*0.25),y+(currentHeight*0.25), currentWidth*0.5, currentHeight*0.5);
-            // ellipse(x+(currentWidth*0.5),y+(currentHeight*0.5), PI);
-    
-            x += currentWidth;
+            stroke(255);
+            strokeWeight(10);
+
+            rect(x, y, currsize, rectH);
+
+            noStroke();
+            fill(random(colours));
+
+
+            rect(x + (currsize * 0.25), y + (currsize * 0.25), currsize * 0.5, currsize * 0.5);
+
+
+            x += currsize;
             let availableWidth = width - x;
-            currentWidth = random(sizes);
-            
-            
-            // makes sure the cell at the edge only occupies the availabe space, so the boarder shows and doesnt get cut off
-            while(currentWidth > availableWidth){
-                currentWidth -= CELL_SIZE;
+            currsize = random(sizes);
+
+
+            // makes sure the cell at the edge only occupies the available space, so the border shows and doesn't get cut off
+            while (currsize > availableWidth) {
+                currsize -= CELL_SIZE;
             }
         }
-        y += currentHeight;
-        let availabeHeight = height - y;
-        currentHeight = random(sizes);
-        while(currentHeight > availabeHeight){
-            currentHeight -= CELL_SIZE;
+        y += currsize1;
+        let availableHeight = height - y;
+        currsize1 = random(sizes);
+        while (currsize1 > availableHeight) {
+            currsize1 -= CELL_SIZE;
         }
         x = 0;
     }
-}
-function draw() {
-
 }
